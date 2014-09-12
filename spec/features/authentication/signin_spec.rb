66678c4dca336d3c56caf5c4d991d2c1a34356cd
bahelms <jimbonkgit@gmail.com>
2014-09-12 18:01:14 -0400

    adds signin feature spec

---
+++ spec/features/authentication/signin_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+require "support/authentication"
+
+feature "Signing in with an existing account" do
+  let(:user) { create(:user) }
+  subject { page }
+  background { visit root_path }
+
+  it { should have_content("Sign In") }
+  it { should have_field("Email") }
+  it { should have_field("Password") }
+
+  context "with valid input" do
+    scenario "successfully signs in user if email and password are valid" do
+      sign_in_with email: user.email, password: user.password
+      expect(page).not_to have_content("Invalid email or password")
+    end
+
+    feature "Signing out of a valid session" do
+      background do
+        sign_in_with email: user.email, password: user.password
+        click_link "Sign Out"
+      end
+
+      it { should_not have_link "Sign Out" }
+      it { should have_button "Sign In" }
+    end
+  end
+
+  context "with invalid email or password" do
+    scenario "notifies user if the email is invalid" do
+      sign_in_with email: nil, password: user.password
+      expect(page).to have_content("Invalid email or password")
+    end
+
+    scenario "notifies user if the password is invalid" do
+      sign_in_with email: user.email, password: "waht_me_worry?"
+      expect(page).to have_content("Invalid email or password")
+    end
+  end
+end
+
--- spec/support/authentication.rb
+++ spec/support/authentication.rb
@@ -7,3 +7,20 @@ def signup_with(options)
   fill_in "Confirm password", with: confirm
   click_button "Sign Up"
 end
+
+def sign_in_with(options)
+  fill_in "Email", with: options[:email]
+  fill_in "Password", with: options[:password]
+  click_button "Sign In"
+end
+
+def update_user(options)
+  confirm = options[:confirm] ? options[:confirm] : options[:new_password]
+
+  fill_in "Email", with: options[:email]
+  fill_in "Current password", with: options[:password]
+  fill_in "New password", with: options[:new_password]
+  fill_in "Confirm password", with: confirm
+  click_button "Update"
+end
+
