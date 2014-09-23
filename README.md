e63cfe40c181744bf4fd0c89f9f47596e4fbffe2
bahelms <jimbonkgit@gmail.com>
2014-09-23 18:02:50 -0400

    adds more password tests

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ This app is my homemade, free version of MTG Online.
 
 ###TODO
 
+* Add new reset_password_spec tests to base_app
+
 * Lobby chat
 * Deck builder
 * Game engine
--- spec/features/devise/reset_password_spec.rb
+++ spec/features/devise/reset_password_spec.rb
@@ -3,6 +3,7 @@ require "support/authentication"
 
 feature "Reseting password" do
   let(:user) { create(:user) }
+  let(:mail) { ActionMailer::Base.deliveries.first }
   subject { page }
 
   background do
@@ -10,10 +11,24 @@ feature "Reseting password" do
     click_link "Forgot your password?"
   end
 
-  scenario "sends the user an email with a reset password link" do
-    fill_in "Email", with: user.email
-    expect {
+  context "with valid email address" do
+    background { fill_in "Email", with: user.email }
+
+    scenario "sends the user an email with a reset password link" do
+      expect {
+        click_button "Send reset password instructions"
+      }.to change(ActionMailer::Base.deliveries, :count).by(1)
+    end
+
+    scenario "sends an email to the address provided" do
       click_button "Send reset password instructions"
-    }.to change(ActionMailer::Base.deliveries, :count).by(1)
+      expect(mail.to).to eq [user.email]
+    end
+  end
+
+  context "with nonexistant email address" do
+    scenario "displays error message" do
+      pending
+    end
   end
 end
