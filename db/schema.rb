f5a985cb25f452bcf24378250ba160eb0ce8bb19
bahelms <jimbonkgit@gmail.com>
2014-10-23 22:09:17 -0400

    changes ability effect to array

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141021105947) do
+ActiveRecord::Schema.define(version: 20141024013640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,9 +20,9 @@ ActiveRecord::Schema.define(version: 20141021105947) do
   create_table "abilities", force: true do |t|
     t.string "name"
     t.text   "trigger"
-    t.text   "effect"
-    t.string "type",    null: false
+    t.string "type",                 null: false
     t.hstore "cost"
+    t.string "effects", default: [],              array: true
   end
 
   create_table "abilities_cards", id: false, force: true do |t|
