f275fae252c81bfbf3f53d80d71fe91db4a317ac
bahelms <jimbonkgit@gmail.com>
2014-12-09 20:38:25 -0500

    really fixes AMS embed ids

--- app/serializers/card_serializer.rb
+++ app/serializers/card_serializer.rb
@@ -1,9 +1,9 @@
 class CardSerializer < ActiveModel::Serializer
   attributes :id, :name, :mana_cost, :colors, :rarity, :power, :toughness,
     :dual_card, :card_set, :type, :supertype, :type_class
-  has_many :keyword_abilities, embed: :ids
-  has_many :abilities, embed: :ids
-  has_many :subtypes, embed: :ids
+  has_many :keyword_abilities, embed: :ids, include: true
+  has_many :abilities, embed: :ids, include: true
+  has_many :subtypes, embed: :ids, include: true
 
   def card_set
     object.card_set.name
